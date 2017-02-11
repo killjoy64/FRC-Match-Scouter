d69c5f9be0f07e46b39cb6d7840ed9bb181c3966
Kyle Flynn <kyle.flynn5798@yahoo.com>
2017-02-11 02:30:03 -0500

    Added native functions and UI updates.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,22 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': '9c6b24fe'
+  }
+};
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {File, FileEntry, Metadata} from 'ionic-native';
+import {AlertController, ToastController, Platform} from 'ionic-angular';
 
-import { NavController } from 'ionic-angular';
+declare var cordova: any;
 
 @Component({
   selector: 'page-home',
@@ -8,12 +10,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  IS_BROWSER: boolean;
+
   mode: string;
 
   scouter_name: string;
 
   team_number: number;
   team_alliance: boolean;
+  team_alliance_string: string;
   match_number: number;
 
   auto_baseline: boolean;
@@ -31,10 +36,43 @@ export class HomePage {
 
   comments: string;
 
-  constructor(public navCtrl: NavController) {
+  fs: string;
+  p: string;
+
+  scouting_files: any;
+
+  constructor(private platform: Platform, private alertCtrl: AlertController, private toastCtrl: ToastController) {
     this.mode = "scouting";
 
     this.resetFields();
+
+    if (typeof cordova == 'undefined') {
+      this.IS_BROWSER = true;
+
+      console.log("BROWSER DETECTED. NATIVE FEATURES DISABLED.");
+    } else {
+      this.IS_BROWSER = false;
+
+      if (this.platform.is("android")) {
+        this.fs = cordova.file.externalDataDirectory;
+        this.p = "ANDROID";
+      } else if (this.platform.is("ios")) {
+        this.fs = cordova.file.documentsDirectory;
+        this.p = "ANDROID";
+      } else if (this.platform.is("windows")) {
+        this.fs = cordova.file.dataDirectory;
+        this.p = "WINDOWS";
+      }
+
+      console.log("MAPPED DIRECTORIES FOR " + this.p);
+    }
+
+    this.scouting_files = null;
+
+  }
+
+  ionViewWillEnter() {
+    this.getScoutingForms();
   }
 
   resetFields() {
@@ -42,6 +80,7 @@ export class HomePage {
 
     this.team_number = null;
     this.team_alliance = false;
+    this.team_alliance_string = "red";
     this.match_number = null;
 
     this.auto_gear = null;
@@ -110,11 +149,169 @@ export class HomePage {
   }
 
   submitForm() {
-    console.log("CONGRATULATIONS YOU SUBMITTED A FORM IM SO PROUD OF YOU I HATE MY LIFE AND THIS PROGRAM");
-    this.resetFields();
-    setTimeout(() => {
+
+    let msg = null;
+
+    if (!this.scouter_name) {
+      msg = "you must enter your name.";
+      this.presentAlert("Missing Fields", "There are empty fields on the form, " + msg);
+      return;
+    }
+
+    if (!this.team_number) {
+      msg = "you must enter a team number.";
+      this.presentAlert("Missing Fields", "There are empty fields on the form, " + msg);
+      return;
+    }
+
+    if (!this.match_number) {
+      msg = "you must enter a match number.";
+      this.presentAlert("Missing Fields", "There are empty fields on the form, " + msg);
+      return;
+    }
+
+    if (!this.auto_gear) {
+      msg = "you must choose an option for the gear in autonomous. ";
+      this.presentAlert("Missing Fields", "There are empty fields on the form, " + msg);
+      return;
+    }
+
+    if (!this.tele_climb) {
+      msg = "you must choose an option for climbing in teleop.";
+      this.presentAlert("Missing Fields", "There are empty fields on the form, " + msg);
+      return;
+    }
+
+    if (!this.driver_rating) {
+      msg = "you must choose a driver rating. Remember to be honest!";
+      this.presentAlert("Missing Fields", "There are empty fields on the form, " + msg);
+      return;
+    }
+
+    if (!this.driver_defense_rating) {
+      msg = "you must choose a driver defense rating.";
+      this.presentAlert("Missing Fields", "There are empty fields on the form, " + msg);
+      return;
+    }
+
+    this.exportForm().then((file_name) => {
+      let toast = this.toastCtrl.create({
+        message: 'Successfully exported scouting form ' + file_name,
+        duration: 3000,
+        showCloseButton: true
+      });
+
+      toast.present().then(() => {
+        this.resetFields();
+        this.mode = "exporting";
+      });
+    }).catch((err) => {
+      console.log("Error exporting form. " + err.message + " " + err.code);
+      this.resetFields();
       this.mode = "exporting";
-    }, 100);
+    });
+
+  }
+
+  presentAlert(title, msg) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      message: msg,
+      buttons: [
+        {
+          text: 'Dismiss',
+          role: 'cancel',
+          handler: () => {}
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  getScoutingForms() {
+    this.getFiles().then((entries:FileEntry[]) => {
+      this.scouting_files = entries;
+      for (let i = 0; i < entries.length; i++) {
+        entries[i].getMetadata((metadata:Metadata) => {
+          this.scouting_files[i].size = metadata.size;
+          this.scouting_files[i].modificationTime = metadata.modificationTime;
+          console.log(this.scouting_files[i]);
+        }, (err) => {
+          console.log(this.scouting_files[i] + " error getting metadata. " + err.message);
+        });
+      }
+      console.log(this.scouting_files.length + " scouting forms found.");
+    }).catch((err) => {
+      console.log("Error getting scouting forms. " + err.message + " " + err.code);
+    });
+  }
+
+  exportForm() {
+
+    if (this.team_alliance) {
+      this.team_alliance_string = "blue";
+    } else {
+      this.team_alliance_string = "red";
+    }
+
+    let scouting_form = {
+      general: {
+        name: this.scouter_name,
+        team_number: this.team_number,
+        match_number: this.match_number
+      },
+      autonomous: {
+        gear: this.auto_gear,
+        baseline: this.auto_baseline,
+        high_fuel: this.auto_high_fuel,
+        low_fuel: this.auto_low_fuel
+      },
+      teleop: {
+        gears: this.tele_gears,
+        high_fuel: this.tele_high_fuel,
+        low_fuel: this.tele_low_fuel,
+        climb: this.tele_climb
+      },
+      driver_rating: this.driver_rating,
+      driver_defense_rating: this.driver_defense_rating,
+      comments: this.comments
+    };
+
+    console.log(scouting_form);
+
+    if (!this.IS_BROWSER) {
+      return new Promise((resolve, reject) => {
+        File.writeFile(this.fs, this.match_number + "-" + this.team_number + ".json", JSON.stringify(scouting_form), {replace: true}).then((entry) => {
+          console.log("Successfully created file " + entry.name);
+          this.getScoutingForms();
+          resolve(entry.name);
+        }, (err) => {
+          reject(err);
+        });
+      });
+    } else {
+      return new Promise((resolve, reject) => {
+        reject({
+          code: "APP_BROWSER_MODE_ON",
+          message: "Could not write file. Browser detected. Native features have been disabled."
+        });
+      });
+    }
+  }
+
+  getFiles() {
+    if (!this.IS_BROWSER) {
+      return File.listDir(this.fs, "");
+    } else {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          reject({
+            code: "APP_CONFIG_BROWSER_MODE",
+            message: "Could not list directory contents. Browser detected. Native features have been disabled."
+          });
+        }, 100);
+      });
+    }
   }
 
 }
